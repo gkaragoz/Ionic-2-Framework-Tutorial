f28600430c22b8e4e17d0825d041391651d4eed1
gkaragoz <annotto3@gmail.com>
2017-03-06 15:02:37 +0300

    Add details page.

---
+++ Ion-Reddit/src/pages/details/details.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  templateUrl: 'details.html'
+})
+export class DetailsPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
